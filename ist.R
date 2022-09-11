5234fb477d0e00de0f1160920a24526d90aa3c19
Godwin  Osuji <godwinosuji2013@gmail.com>
2022-09-11 12:48:01 +0100

    Update ist.R

--- ist.R
+++ ist.R
@@ -27,7 +27,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 pollutantmean("specdata", "nitrate", 1)
 
 
-The function ‘complete’ reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases.
+The function ‘complete’ reads a directory full of files and reports 
+the number of completely observed cases in each data file. The function 
+  should return a data frame where the first column is the name of 
+the file and the second column is the number of complete cases.
 
 complete <- function(directory, id = 1:332){
   ## 'director' is a character vector of length 1 indicating
@@ -54,7 +57,12 @@ complete <- function(directory, id = 1:332){
   }
   results
 }
-The function ‘corr’ takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0.
+The function ‘corr’ takes a directory of data files and a threshold for complete 
+  cases and calculates the correlation between sulfate and nitrate for monitor 
+  locations where the number of completely observed cases (on all variables) is 
+greater than the threshold. The function should return a vector of correlations 
+for the monitors that meet the threshold requirement. If no monitors meet the 
+threshold requirement, then the function should return a numeric vector of length 0.
 
 corr <- function(directory, threshold = 0){
   ## 'directory' is a character vector of length 1 indicating
