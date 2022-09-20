a4689f4bf9959a48ab0f829391c448c04aa8d1bb
sirgo1 <godwinosuji2013@gmail.com>
2022-09-20 03:01:40 +0100

    deleted

--- practice4.R
+++
@@ -1,5 +0,0 @@
-#SUMMARIZING DATA
-download.file("https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD",
-              "restaurants.csv", method = "curl") 
-restData <- read.csv("restaurants.csv")
-head(restData)
